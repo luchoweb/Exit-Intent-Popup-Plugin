929737ec64ae2195029ff28d015837bcb6324b40
Lucho Web <luchoweb.dev@gmail.com>
2023-11-27 20:54:28 -0500

    performance improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const minify = require("gulp-minify");
+const cleanCss = require('gulp-clean-css');
 const browserSync = require("browser-sync").create();
 
 const localDomain = "lucho.local";
@@ -8,6 +10,10 @@ const publicPaths = {
     src: "./public/assets/scss/*.scss",
     dest: "./public/assets/css/",
   },
+  js: {
+    src: "./public/assets/js/*.js",
+    dest: "./public/assets/js/",
+  },
 };
 
 // Compile CSS
@@ -15,9 +21,19 @@ gulp.task("public-scss", function () {
   const stream = gulp
     .src([publicPaths.scss.src])
     .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCss())
     .pipe(browserSync.stream());
 
   return stream.pipe(gulp.dest(publicPaths.scss.dest));
+});
+
+gulp.task("minify-js", function () {
+  const stream = gulp
+    .src([publicPaths.js.src])
+    .pipe(minify())
+    .pipe(browserSync.stream());
+
+  return stream.pipe(gulp.dest(publicPaths.js.dest));
 });
 
 gulp.task("serve", function () {
